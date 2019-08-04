44558c39ae3b71163f08973c1769ca60205e954f
Dethrowe <dethrowe@pop-os.localdomain>
2019-08-04 10:38:39 -0400

    finished the this follow along

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,64 +1,115 @@
-import React, { Component } from 'react';
-import Joke from './Joke';
-import axios from 'axios';
-import uuid from 'uuid/v4';
-import './JokeList.css';
+import React, { Component } from "react";
+import Joke from "./Joke";
+import axios from "axios";
+import uuid from "uuid/v4";
+import "./JokeList.css";
 
 export class JokeList extends Component {
     static defaultProps = {
         numJokesToGet: 10
-    }
+    };
     constructor(props) {
         super(props);
-        this.state = { 
-            jokes: [] 
-        }
+        this.state = {
+            //Grabs jokes from local storage if exist and converts them from string to obj. Or gives us empty array to wrk with
+            //This allows us to see the same jokes even after page load.
+            jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+            loading: false
+        };
+        this.seenJokes = new Set(this.state.jokes.map(j => j.text)); //Store text of jokes in a set to make sure we do not get duplicates.
     }
 
-    async componentDidMount(){
-        let jokes = [];
-        while(jokes.length < this.props.numJokesToGet){
-            //Load jokes
-            //Data defaults to return as type HTML, so we have to request json wit headers
-            let res = await axios.get("https://icanhazdadjoke.com/", {
-                headers: {Accept: "application/json"}
-            });
-            jokes.push({id: uuid (), text: res.data.joke, votes: 0})
+    componentDidMount() {
+        if (this.state.jokes.length === 0) this.getJokes();
+    }
+
+    async getJokes() {
+        try {
+            let jokes = [];
+            while (jokes.length < this.props.numJokesToGet) {
+                //Load jokes
+                //Data defaults to return as type HTML, so we have to request json wit headers
+                let res = await axios.get("https://icanhazdadjoke.com/", {
+                    headers: { Accept: "application/json" }
+                });
+                let newJoke = res.data.joke;
+                //Check to see if that joke is in our set.
+                if (!this.seenJokes.has(newJoke)) {
+                    jokes.push({ id: uuid(), text: newJoke, votes: 0 }); //Here after we pull in the jokes from the API we assign it along with an id and votes to an empty array.
+                }
+            } //This callback (st) allows us to use the current state and add new jokes to it.
+            this.setState(st => ({
+                loading: false,
+                jokes: [...st.jokes, ...jokes]
+            }));
+            window.localStorage.setItem("jokes", JSON.stringify(jokes)); //Setting jokes to local storage, and it only accepts a string.
+        } catch (e) {
+            alert(e);
+            this.setState({ loading: false });
         }
-        this.setState({ jokes: jokes })
     }
 
     //Function handles the up or down votes on the jokes
-    handleVote(id, delta){//Delta is a neg or pos number that gets passed to the function.
-        this.setState(st => ({//Since we are incrementing and decrementing we have to use the callback version of setState
-                jokes: st.jokes.map(j => 
-                    j.id === id ? {...j, votes: j.votes + delta} : j
+    handleVote(id, delta) {
+        //Delta is a neg or pos number that gets passed to the function.
+        this.setState(
+            //This callback (st) allows us to use the current state and add new jokes to it.
+            st => ({
+                //Since we are incrementing and decrementing we have to use the callback version of setState
+                jokes: st.jokes.map(j =>
+                    j.id === id ? { ...j, votes: j.votes + delta } : j
                 )
-            }));
+            }),
+            () =>
+                window.localStorage.setItem(
+                    "jokes",
+                    JSON.stringify(this.state.jokes)
+                )
+        ); //This is an anom function called after the handleVote happens.
     }
 
+    handleClick = () => {
+        this.setState({ loading: true }, this.getJokes); //On click it will set loading to true THEN get the jokes.
+    };
+
     render() {
+        if (this.state.loading) {
+            return (
+                <div className="JokeList-spinner">
+                    <i className=" far fa-8x fa-laugh fa-spin" />
+                    <h1 className="JokeList-title">Loading...</h1>
+                </div>
+            );
+        }
+        let jokes = this.state.jokes.sort((a,b) => b.votes - a.votes)//Sorts by votes by subtracting b.votes from a.votes
         return (
             <div className="JokeList">
-            <div className="JokeList-sidebar">
-                <h1 className="JokeList-title">
-                <span>Dad</span> Jokes
-                </h1>
-                <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
-                <button className="JokeList-getmore">New Jokes</button>
-            </div>
+                <div className="JokeList-sidebar">
+                    <h1 className="JokeList-title">
+                        <span>Dad</span> Jokes
+                    </h1>
+                    <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
+                    <button
+                        className="JokeList-getmore"
+                        onClick={this.handleClick}
+                    >
+                        New Jokes
+                    </button>
+                </div>
                 <div className="JokeList-jokes">
-                    {this.state.jokes.map( j => (
-                        <Joke key={j.id} 
-                        votes={j.votes} 
-                        text={j.text} 
-                        upVote={() => this.handleVote(j.id, 1)} 
-                        downVote={() => this.handleVote(j.id, -1)}/>
+                    {jokes.map(j => (//Map over our sorted jokes.
+                        <Joke
+                            key={j.id}
+                            votes={j.votes}
+                            text={j.text}
+                            upVote={() => this.handleVote(j.id, 1)}
+                            downVote={() => this.handleVote(j.id, -1)}
+                        />
                     ))}
                 </div>
             </div>
-        )
+        );
     }
 }
 
-export default JokeList
+export default JokeList;
